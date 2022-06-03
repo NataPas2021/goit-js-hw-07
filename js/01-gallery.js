ec4a11e5c0a1b1ab065a0ee3dc2e398d88911279
NataPas2021 <94239256+NataPas2021@users.noreply.github.com>
2022-06-04 02:14:35 +0300

    gallery markup is done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+const imagesMarkup = createGalleryItemsList(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', imagesMarkup);
+galleryContainer.addEventListener('click', onGalleryContainerClick)
+
+function createGalleryItemsList(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+           <div class="gallery__item">
+             <a href="${original}" class="gallery__link">
+              <img 
+              class="gallery__image"
+              src = "${preview}"
+              data-source = "${original}"
+              alt = "${description}"
+              />
+             </a>
+           </div>
+        `;
+    })
+    .join('');
+
+}
+
+function onGalleryContainerClick(e) {
+    if(!e.target.classList.contains('gallery__image')) {
+        return;
+    }
+    console.log(e.target);
+
+}
+
+
 console.log(galleryItems);
