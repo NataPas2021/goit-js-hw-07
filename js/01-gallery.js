60700fcc7f1bece0d99c53e88458ac3c45a4b68d
NataPas2021 <94239256+NataPas2021@users.noreply.github.com>
2022-06-07 13:33:51 +0300

    task 1 -done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -27,12 +27,27 @@ function createGalleryItemsList(galleryItems) {
 }
 
 function onGalleryContainerClick(e) {
+    e.preventDefault();
     if(!e.target.classList.contains('gallery__image')) {
         return;
     }
     
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`)
 
+instance.show()
+
+if(instance.visible) {
+    window.addEventListener('keydown', (e) => {
+        if(e.key === 'Escape') {
+            instance.close();
+    }
+    
+    }, {once: true})
 }
+
+};
 
 
 console.log(galleryItems);
